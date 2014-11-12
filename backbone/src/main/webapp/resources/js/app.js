0103e44dadbac42248a3aae483cf3c2d1fc34773
dimonn12 <dimonn12@hotmail.com>
2014-11-12 22:31:45 +0300

    app.js initial

--- backbone/src/main/webapp/resources/js/app.js
+++ backbone/src/main/webapp/resources/js/app.js
@@ -1,10 +1,140 @@
-var TagModel = Backbone.Model.extend({
-	urlRoot: 'http://localhost:8080/BookmarkApp/tag',
-	url: function() {
-		return this.urlRoot;
-	}
+var bookmarkApp = bookmarkApp || {};
+
+bookmarkApp.routers = bookmarkApp.routers || {};
+bookmarkApp.models = bookmarkApp.models || {};
+bookmarkApp.collections = bookmarkApp.collections || {};
+bookmarkApp.views = bookmarkApp.views || {};
+
+bookmarkApp.routers.AppRouter = Backbone.Router.extend({
+    routes: {
+     '*other' : 'defaultRoute'   
+    },
+    
+    defaultRoute: function(other) {
+        console.log("Invalid path " + other);   
+    }
 });
-model = new TagModel();
-model.fetch();
-//model.set('tag', 'HAHAHA');
-model.save();
+
+bookmarkApp.models.BookmarkModel = Backbone.Model.extend({
+    
+});
+
+bookmarkApp.models.TagModel = Backbone.Model.extend({
+
+});
+
+bookmarkApp.models.TagCountModel = Backbone.Model.extend({
+    
+});
+
+bookmarkApp.collections.TagCountListCollection = Backbone.Collection.extend({
+    
+});
+
+bookmarkApp.collections.TagListCollection = Backbone.Collection.extend({
+    model: bookmarkApp.models.TagModel,
+    url: 'tag'
+});
+
+bookmarkApp.collections.BookmarkListCollection = Backbone.Collection.extend({
+    
+});
+
+bookmarkApp.views.BookmarkFormView = Backbone.View.extend({
+    
+    initialize: function() {
+        
+    },
+    
+    render: function() {
+        
+        return this;
+    }
+});
+
+bookmarkApp.views.BookmarkListContainerView = Backbone.View.extend({
+    
+    initialize: function() {
+        
+    },
+    
+    render: function() {
+        
+        return this;
+    }
+});
+
+bookmarkApp.views.BookmarkView = Backbone.View.extend({
+    
+    initialize: function() {
+        
+    },
+    
+    render: function() {
+        
+        return this;
+    }
+});
+
+bookmarkApp.views.BookmarkListView = Backbone.View.extend({
+    
+    initialize: function() {
+        
+    },
+    
+    render: function() {
+        
+        return this;
+    }
+});
+
+bookmarkApp.views.TagCountListView = Backbone.View.extend({
+    
+    initialize: function() {
+        
+    },
+    
+    render: function() {
+        
+        return this;
+    }
+});
+
+bookmarkApp.views.TagCountView = Backbone.View.extend({
+    
+    initialize: function() {
+        
+    },
+    
+    render: function() {
+        
+        return this;
+    }
+});
+
+
+
+var TagCountView = Backbone.View.extend({
+    initialize: function() {
+        
+    },
+    
+    render: function() {
+        
+        return this;
+    }
+});
+
+Backbone.history.start({
+    pushState: true/false,
+    root: ''
+});
+
+$(document).ready(function() {
+   var tagCollection = new bookmarkApp.collections.TagListCollection();
+    tagCollection.fetch({
+    success: function(collection) {
+      console.log(collection);  
+    }
+    });
+});
